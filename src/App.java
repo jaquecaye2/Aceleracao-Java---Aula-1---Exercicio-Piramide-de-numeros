4511fafad58e0ab9d3a55a9610dbff0af4916f56
jaquecaye <jaquelinecaye@gmail.com>
2023-01-09 18:39:47 -0300

    Resolving activity

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        int valorFinal = 5;
+
+        for (int i = 1; i <= valorFinal; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println(" ");
+        }
+
+    }
+
+}
